21578891d65b33a9ae2cace068135e82be43390b
Emilien Domenge <emidomh@gmail.com>
2020-07-12 15:14:58 +0200

    Add app component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
+import { connect4 } from './settings/index';
+import { AppState } from './ngxs/index';
 import { ThemingService } from './shared/services/theming/theming.service';
 import { NgxsModule, Store } from '@ngxs/store';
 import RootState from 'src/app/ngxs/state/root.state';
@@ -32,5 +34,47 @@ describe('AppComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.nativeElement;
         expect(compiled.querySelectorAll('app-toolbar').length).toEqual(1);
+    });
+
+    it('should render banner info', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelectorAll('app-banner-info').length).toEqual(1);
+    });
+
+    it('should render a board', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelectorAll('app-board').length).toEqual(1);
+    });
+
+    it('should render a signature footer', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelectorAll('app-signature').length).toEqual(1);
+    });
+
+    it('should render a sidenav', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelectorAll('app-signature').length).toEqual(1);
+    });
+
+    it('should have default ngxs store', () => {
+        const ngxsState = store.selectSnapshot((state) => state);
+        const { currentBoard, playerPlaying, winConditionResolved, gameOver, winner } = ngxsState.connect4;
+        expect(currentBoard.length).toEqual(connect4.nbColumns * connect4.nbRows);
+
+        // all values should be null
+        expect(currentBoard.filter((i) => i === null).length).toEqual(connect4.nbColumns * connect4.nbRows);
+
+        expect(playerPlaying).toEqual(null);
+        expect(winConditionResolved).toEqual(null);
+        expect(gameOver).toEqual(false);
+        expect(winner).toEqual(null);
     });
 });
